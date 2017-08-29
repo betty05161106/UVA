13c5e0a81b4e6d81b1f24d2418fa047b7d5e42de
betty05161106 <betty05161106@gmail.com>
2017-08-29 13:59:19 +0800

    Update UVA488.cpp

--- UVA488.cpp
+++ UVA488.cpp
@@ -1,34 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 #include <assert.h>
 #include <string>
 using namespace std;
 
 void wave(int amplitude, int frequency)
 {
-	assert(amplitude <= 9);
-	for (int i = 1; i <= frequency; i++) {
-		for (int j = 1; j <= amplitude; j++) {
-			cout << string(j, j + '0') << endl;
-		}
-		for (int j = amplitude-1; j >= 1; j--) {
-			cout << string(j, j + '0') << endl;
-		}
-		cout << endl;
-	}
+    assert(amplitude <= 9);
+    while (frequency--) {
+        for (int j = 1; j <= amplitude; j++) {
+            cout << string(j, '0' + j) << endl;
+        }
+        for (int j = amplitude-1; j >= 1; j--) {
+            cout << string(j, '0' + j) << endl;
+        }
+        if (frequency) cout << endl;
+    }
 }
 
 int main()
 {
-	int n;
-	int amplitude;
-	int frequency;
-	cin >> n; 
-
-	for (int i = 1; i <= n; i++) {
-		cin >> amplitude;
-		cin >> frequency;
-		wave(amplitude, frequency);
-	}
-	system("pause");
-	return 0;
+    int n;
+    int amplitude;
+    int frequency;
+    cin >> n; 
+    
+    while (n--) {
+        cin >> amplitude >> frequency;
+        wave(amplitude, frequency);
+    }
+    system("pause");
+    return 0;
 }
